441eeaa3936168d87806e66baa2710c80a90294e
kkumarasamy <kkumarasamy@edit-place.com>
2019-02-22 20:32:38 +0530

    Added vue.config.js & Updated main.js for router configuration

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ Vue.use(VueRouter);
 
 export const router = new VueRouter({
   mode: 'history',
+  base: process.env.BASE_URL,
   routes: [
     { path: '/', component: ImageList },
     { path: '/upload', component: UploadForm },
